94bcdff8f15f365ac0559046513102ceab3a8385
Frini Panteliadi <frini.panteliadi@gmail.com>
2018-03-07 13:08:56 +0200

    Added one more case to the insertion function

--- testing.c
+++ testing.c
@@ -2,54 +2,40 @@ typedef struct trieNode{
 	char letter;					/*Letter stored inside the node*/
 	bool isEndOfWord;				/*Flag which indicated whether a word ends in the current node*/
 	struct trieNode **children;		/*An array of pointers, where each one will store the address of a child node*/
-	// postingsList *listPtr;			/*Pointer to postings list of a leaf node*/
+	// postingsList *listPtr;		/*Pointer to postings list of a leaf node*/
 	int arraySize;					/*Size of the array of pointers (needed for use with realloc)*/
 	int childNodes;					/*Indicates the number of children the node has*/
 }trieNode;
 
 
-/* Creating the root of the Trie data structure */
+/* Creating the root of the Trie data structure
+   and initializing its members */
 int createRoot(trieNode *root){
 	
-	/* Create the root of the Trie */
 	root = (trieNode*)malloc(sizeof(trieNode));
 	if(root == NULL)
 		return -1;
 
-	/* Initialize the root */
 	root->letter = 0;
 	root->isEndOfWord = False;
 	// root->listPtr = NULL;
 	root->arraySize = 0;
 	root->childNodes = 0;
-
 	root->children = NULL;
 }
 
-/* Compares the ASCII value between two characters.
-   Helps with finding the right position in the Trie.
-
-   Possible return values: -1, 0, 1 */
+/* Returns the difference between the ASCII values
+   of a and b */
 int compareKeys(char* a, char* b){
-
-	/*In terms of ASCII VALUE: */
-	
-	/* a precedes b */
-	if((int)(*a) < (int)(*b))
-		return -1;
-
-	/* a and b have the same value */
-	else if((int)(*a) == (int)(*b))
-		return 0;
-
-	/* b precedes a */
-	else if((int)(*a) > (int)(*b))
-		return 1;
+	return(((*a)-(*b)));
 }	
 
+/* Inserts a new value (word) in the Trie */
 int insertTrie(trieNode* node, char* word){
 	trieNode* temp = node;
 
+	/* Each iteration is responsible for a single
+	   letter of the word we're trying to insert */
 	for(int i=0; i < strlen(word); i++){
 
 		/* The node we're currently working on doesn't have
@@ -102,16 +88,24 @@ int insertTrie(trieNode* node, char* word){
 		   one child */
 		else{
 			
-			/*Find the correct placement for the new letter*/
-			int j;
-			for(j=0; j < (temp->childNodes); j++){
+			/* We need to find the correct placement 
+			   for the letter we are trying to insert */
+			int j = 0;
+			int value = 1;
+			while((value!=0) && j<(temp->childNodes)){
+				value = compareKeys(word[i],(temp->children[j]->letter));
+				if(value < 0)
+					j++;
+			}
+			/*for(j=0; j < (temp->childNodes); j++){
 				int value = compareKeys(&(temp->children[j]->letter),&word[i]);
 
 				if(value == -1)
 					continue;
 				else if(value == 1)
-			}
+			}*/
 
+			/* The node already exists */
 			if(temp->children[j] == NULL){
 				temp->childNodes++;	
 
@@ -121,17 +115,23 @@ int insertTrie(trieNode* node, char* word){
 
 				temp->children[j]->letter = word[i];
 
-				if(i<strlen(word)-1){
+				/* The letter we just inserted does not
+				   indicate the end of a word */
+				if(i < (strlen(word)-1)){
 					temp->children[j]->isEndOfWord = False;
 					// temp->children[j]->listPtr = NULL;
 				}
-				// else{}
 
+				/* The current node doesn't have any children.
+				   That's why we must initialize the following 
+				   values with zero */
 				temp->children[j]->arraySize = 0
 				temp->children[j]->childNodes = 0;
 				temp->children[j]->children = NULL;
 			}
 
+			/* If we still have letters that we need to insert,
+			   we must change the node that we're working on */
 			if(i < strlen(word)-1){
 				temp = temp->children[j];
 				continue;
